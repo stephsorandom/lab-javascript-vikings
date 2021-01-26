58a4db36ea3a4154678f0e4ba63bb1fa9a9cb280
Stephanie Mitchell <stephsorandom@gmail.com>
2021-01-26 17:43:07 -0500

    sudden all failures..facepalm

--- src/viking.js
+++ src/viking.js
@@ -65,23 +65,56 @@ class War {
    addViking(pike) {
     this.Viking.push(pike);
    } 
+
    addSaxon(arrow) {
     this.Saxon.push(arrow);
    }
+
    vikingAttack() {
+    let luckySoldier = getRandomInt(this.vikingArmy.length)
+    let unluckySoldier = getRandomInt(this.saxonArmy.length)
+    this.saxonArmy[unluckySoldier].receiveDamage(this.vikingArmy[luckySoldier].strength)
+   
+    if(this.saxonArmy[unluckySoldier].health <=0) {
+        this.saxonArmy.splice(unluckySolider,1);
+        return `A Saxon has died in combat`
+    } else {
+        return `A Saxon has received ${this.vikingArmy[luckySoldier].strength} of damage!`
+    }
+    }
 
-   }
+
+
    saxonAttack() {
-
+    let luckySoldier = getRandomInt(this.saxonArmy.length)
+    let unluckySoldier = getRandomInt(this.vikingArmy.length)
+    this.vikingArmy[unluckySoldier].receiveDamage(this.saxonArmy[luckySoldier].strength)
+   
+    if(this.vikingArmy[unluckySoldier].health<=0) 
+    {
+        this.vikingArmy.splice(unluckySolider,1);
+        return `A Viking has fallen`
+    } else {
+        return `${this.vikingArmy[luckySoldier].name} has received ${this.saxonArmy[unluckySoldier].name} of damage!`
+    }
+    }
    }
-   showStatus() {
 
+   
+   showStatus() {
+    if()
    }
 }
 
-let viking = new Viking('John', 250, 60)
-let saxon = new Saxon(69, 100)
+let viking = new Viking ('Ragnar', 300, 300)
+let saxon = new Saxon (200, 250)
+
 let war = new War()
 
 newWar.addViking(viking)
 newWar.addSaxon(saxon)
+
+// newWar.vikingAttack()
+function getRandomInt (max) {
+    return Math.floor(Math.random()*Math.floor(max))
+}
