0c7a125c98ae40270f61baeb5bf1fb00ce73bbed
Stephanie Mitchell <stephsorandom@gmail.com>
2021-01-26 15:43:53 -0500

    I did pretty good alone

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,75 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage (theDamage) {
+         if (this.health -= receiveDamage);
+         return this.health;
+    }   
+}
+    
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+        
+    
+    receiveDamage(theDamage) {
+        if (i=0, theDamage > 0, i++);
+        return this.health;
+    }
+
+    battleCry() {
+        return "Odin Owns You All!"
+    }
+
+}
+
+
+
+
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(theDamage) {
+        if (this.health > 0) {
+            return 'A Saxon has received ${theDamage} points of damage';
+        } else {
+            return 'A Saxon has died in combat';
+        }
+        
+    }
+}
+
+
 
 // War
-class War {}
+class War {
+    vikingArmy = [];
+    saxonArmy = [];
+   addViking(Viking) {
+    this.Viking.push(Viking);
+   } 
+   addSaxon() {
+
+   }
+   vikingAttack() {
+
+   }
+   saxonAttack() {
+
+   }
+   showStatus() {
+
+   }
+}
